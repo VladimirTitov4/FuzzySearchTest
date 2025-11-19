01e6f75b4a67641e99323fbd010dbdb8b4b56eca
Vladimir Titov <v.titov@stbunion.com>
2025-11-19 22:25:33 +0300

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ import me.xdrop.fuzzywuzzy.FuzzySearch;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(FuzzySearch.tokenSetRatio("Vladimir", "Vladimir")); // 100
-        System.out.println(FuzzySearch.tokenSetRatio("Vladimir Ostrovski", "Vladimir Padrino Lopez")); // 62
+        System.out.println(FuzzySearch.tokenSetRatio("Vietnam Vietnam", "vietnam russia joint venture bank")); // 100
+        System.out.println(FuzzySearch.weightedRatio("Vietnam Vietnam", "vietnam russia joint venture bank")); // 100
+//        System.out.println(FuzzySearch.tokenSetRatio("Vladimir Ostrovski", "Vladimir Padrino Lopez")); // 62
     }
 }
